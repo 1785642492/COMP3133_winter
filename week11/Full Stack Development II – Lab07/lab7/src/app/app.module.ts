800480ff2ab9e3463391d777d749c3a196d1e6d2
1785642492 <126036878+1785642492@users.noreply.github.com>
2024-03-24 20:32:38 -0400

    week11

---
+++ week11/Full Stack Development II – Lab07/lab7/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { RemoveSpacesPipe } from './remove-spaces.pipe'; // Import your custom pipe
+import { InputFormatDirective } from './input-format.directive'; // Import your custom directive
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    RemoveSpacesPipe, 
+    InputFormatDirective 
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
